889a73b88cc2778b3945becedf9bb464491c5172
Dorian Zuluaga <166706826+Dorianzuluaga@users.noreply.github.com>
2024-11-23 11:06:48 +0000

    Generate Card Dorian

--- src/app.js
+++ src/app.js
@@ -4,8 +4,39 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { left } from "@popperjs/core";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardGenerator = document.querySelector("#PlayGame");
+  cardGenerator.addEventListener("click", randomCard);
+  const SymbolList = ["♠️", "❤️", "♦️", "♣️", "🗡", "🥳"];
+  const ValueList = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  function randomCard() {
+    let getRandomSymbol =
+      SymbolList[Math.floor(Math.random() * SymbolList.length)];
+    let getRandomValue =
+      ValueList[Math.floor(Math.random() * ValueList.length)];
+
+    document.querySelector("#symbol1").innerHTML = getRandomSymbol;
+    document.querySelector("#value1").innerHTML = getRandomValue;
+    document.querySelector("#value2").innerHTML = getRandomValue;
+    document.querySelector("#symbol2").innerHTML = getRandomSymbol;
+    document.querySelector("#symbol3").innerHTML = getRandomSymbol;
+  }
+  randomCard();
 };
